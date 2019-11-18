649c73cadab15931233bac398351de89ff7a5be0
alvaro <algarecu@gmail.com>
2019-11-18 20:02:41 +0100

    Small bug fixed

--- src/com/wordpress/algarecu/ds/ReplicasBoot.java
+++ src/com/wordpress/algarecu/ds/ReplicasBoot.java
@@ -1,15 +1,67 @@
 package com.wordpress.algarecu.ds;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.CompletableFuture;
+
 import io.atomix.cluster.Member;
 import io.atomix.cluster.Node;
 import io.atomix.core.Atomix;
+import io.atomix.core.lock.DistributedLock;
 import io.atomix.protocols.backup.partition.PrimaryBackupPartitionGroup;
+import io.atomix.protocols.raft.MultiRaftProtocol;
+import io.atomix.protocols.raft.ReadConsistency;
 import io.atomix.protocols.raft.partition.RaftPartitionGroup;
 import io.atomix.cluster.discovery.BootstrapDiscoveryProvider;
 
 
 public class ReplicasBoot {
+	
+	private static void buildLock(Atomix atomix, String filename) {
+		// build the distributed lock for raft protocol and linearizable reads
+		MultiRaftProtocol protocol = MultiRaftProtocol.builder()
+				.withReadConsistency(ReadConsistency.LINEARIZABLE)
+				.build();
+		DistributedLock myLock = atomix.lockBuilder("my-lock")
+				.withProtocol(protocol)
+				.build();
+
+		// get the distributed lock
+		myLock.lock();
+		System.out.println("MyThread" + " : acquiring the file lock...");
+		try {
+			WriteToFile(filename);
+		}
+		finally {
+			myLock.unlock();
+			myLock.close();
+		}
+	}
+
+	private static void WriteToFile(String filename) {
+		// get the date
+		DateFormat dateFormat = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
+		Date today = Calendar.getInstance().getTime();
+		String logDate = dateFormat.format(today);
+
+		// get the writer
+		BufferedWriter writer;
+		try {
+			writer = new BufferedWriter(new FileWriter(filename, true));
+			writer.write(logDate);
+			writer.newLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	public static Member member(String endpoint) {
 		return Member.builder(endpoint)
 				.withProperty("Type", "PERSISTENT")
@@ -21,7 +73,7 @@ public class ReplicasBoot {
 		File dataDir = new File("/tmp/data-local" + "-local-" + System.currentTimeMillis() / 1000);
 		// build the cluster
 		Atomix atomix = Atomix.builder()
-				.withMemberId("member1")
+				.withMemberId("member0")
 				.withHost("localhost").withPort(8000)
 				.withMembershipProvider(BootstrapDiscoveryProvider.builder()
 						.withNodes(
@@ -39,16 +91,23 @@ public class ReplicasBoot {
 								.build())
 						.build())
 				.withManagementGroup(RaftPartitionGroup.builder("system")
-						.withNumPartitions(3)
+						.withNumPartitions(1)
 						.withMembers("member1", "member2", "member3")
 						.withDataDirectory(dataDir)
 						.build())
 				.withPartitionGroups(PrimaryBackupPartitionGroup.builder("data")
-						.withNumPartitions(32)
+						.withNumPartitions(3)
 						.build())
 				.build();
 
-		atomix.start().join();
+		// atomix.start().join();
+
+		CompletableFuture<Void> completableFuture = atomix.start();
+		completableFuture.join();
+
+		// build the lock
+		String filename = "threadsfile.txt";
+		buildLock(atomix, filename);
 	}
 }
 
